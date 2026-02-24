f7a86633e66cfc244a4b6d26ab9b0457b9d2dbcf
Lorena <lfpinones@uc.cl>
2026-02-23 23:54:54 -0300

    sincronizado git con github

---
+++ DOCTOREVALFINAL-M3.py
@@ -0,0 +1,44 @@
+# EVALUACIÓN FINAL MÓDULO 3 - DOCTOR PYTHON
+# programa para registrar, modificar y eliminar horas médicas
+
+# PASO 1. DEFINIR FUNCIONES BÁSICAS
+
+"""
+FUNCIONES:
+registro_hora()
+buscar_hora()
+modificar_hora()
+eliminar_hora()
+
+* en TODAS se busca con RUT
+"""
+
+
+# PASO 2. MENU + INPUTS
+
+"""
+DATOS A PEDIR:
+    RUT
+    Nombre Completo
+    Dirección
+    Edad
+    Comuna
+    Número celular
+    Correo
+    Previsión
+    Fecha
+    Hora
+
+"""
+
+# 1. REGISTRAR HORA
+
+# 2. BUSCAR HORAS
+
+# 3. MODIFICAR HORA
+
+# 4. ELIMINAR HORA
+
+# 5. SALIR DEL PROGRAMA
+
+# llamar menu()
