8d13f5cf87d4c50b9f6c9f165791b5799c498aac
Lorena <lfpinones@uc.cl>
2026-02-24 01:26:21 -0300

    Actualización de README, avance en funciones básicas con leves problemas. Falta función buscar_horas()

--- DOCTOREVALFINAL-M3.py
+++ DOCTOREVALFINAL-M3.py
@@ -18,17 +18,63 @@ def registro_hora():
         "hora": input("Horario de Cita: ")
     }
     lista_horas.append(paciente)
-    print("Cita médica reservada con éxito")
+    print("Cita médica reservada con éxito. \n")
 # buscar_hora()
 def buscar_hora():
-    pass
+    run_buscar = input("Ingrese RUN a buscar: ")
+
+# lista de horas
+def lista_citas():
+    print("- - - - - LISTA   DE   HORAS - - - - -")
+    for paciente in lista_horas:
+        print(f"Paciente: {paciente["nombre"]} - RUN: {paciente["run"]} - Fecha: {paciente["fecha"]}, Hora: {paciente["hora"]}")
+        if paciente == None:
+            print("No hay citas médicas registradas")
+
+
 # modificar_hora()
 def modificar_hora():
-    pass
+    run_modif = input("Ingrese RUN de Paciente: ")
+    for paciente in lista_horas:
+        if paciente["run"] == run_modif:
+            print("¿Desea modificar todos los datos o solo un o en específico?")
+            print("1. Toda la información \n2. Dato en específico")
+            op_cambiar = input("Escoja opción: ")
+            if op_cambiar == "1":
+                paciente["run"] = input("Ingrese RUN: ")
+                paciente["nombre"] = input("Ingrese Nombre: ")               
+                paciente["dirección"] = input("Ingrese Dirección: ")
+                paciente["edad"] = input("Ingrese Edad: ")
+                paciente["comuna"] = input("Ingrese Comuna: ")
+                paciente["número celular"] = input("Ingrese Celular: ")
+                paciente["correo"] = input("Ingrese Correo: ")
+                paciente["previsión"] = input("Ingrese Previsión: ")
+                paciente["fecha"] = input("Ingrese Fecha: ")
+                paciente["hora"] = input("Ingrese Hora: ")
+
+                print("\nDatos actualizados con éxito!")
+
+            elif op_cambiar =="2":
+                dato_modif = input("¿Qué dato desea modificar de la cita?: ")
+                if dato_modif in paciente:
+                    paciente[dato_modif] = input("{dato_modif}: ")
+                    print("Dato actualizado con éxito!")
+            else:
+                print("Opción no válida. Intente nuevamente.")
+        else:
+            print("RUN no encontrado")
+
 # eliminar_hora()
 def eliminar_hora():
-    pass
-# * en TODAS se busca con RUT
+    print("- - - - - ELIMINAR   CITA   MÉDICA - - - - -")
+    run_borrar = input("Ingrese RUN de Paciente: ")
+    for i in range(len(lista_horas)):  #ciclo for para buscar RUN de paciente, i para indexar, len para contabilizar
+        if lista_horas[i]["run"] == run_borrar:
+            lista_horas.pop(i)   #  eliminar hora por indice
+            print("Cita médica eliminada correctamente. \n")
+        else:
+            print("No hay citas médicas registradas para eliminar")
+
 
 # PASO 2. MENU + INPUTS
 
@@ -36,11 +82,11 @@ def menu():
     while True:  # ejecutar el menú hasta que elija opción 5 (salir)
         print("=*" * 5, "D O C T O R   P Y T H O N ", "*=" * 5)
         print("Elija la acción a realizar: ")
-        print("1. Registrar Hora Médica \n" \
-        "2. Buscar Horas Médicas \n" \
-        "3. Modificar Datos Hora \n" \
-        "4. Eliminar Reserva Hora \n" \
-        "5. Salir")
+        print("1. Registrar Hora Médica \n2. Lista de Citas Registradas\n" \
+        "3. Buscar Horas Médicas \n" \
+        "4. Modificar Datos Hora \n" \
+        "5. Eliminar Reserva Hora \n" \
+        "6. Salir")
         opcion = input("Ingrese opción: ")
 
     # 1. REGISTRAR HORA
@@ -48,15 +94,17 @@ def menu():
             registro_hora()
     # 2. BUSCAR HORAS
         elif opcion == "2":
+            lista_citas()
+        elif opcion == "3":
             buscar_hora()
     # 3. MODIFICAR HORA
-        elif opcion == "3":
+        elif opcion == "4":
             modificar_hora()
     # 4. ELIMINAR HORA
-        elif opcion == "4":
+        elif opcion == "5":
             eliminar_hora()
     # 5. SALIR DEL PROGRAMA
-        elif opcion == "5":
+        elif opcion == "6":
             print("\nC e r r a n d o   p r o g r a m a . . .")
             break
         else:
---
+++ README.md
@@ -0,0 +1,16 @@
+# TRABAJO FINAL DE MÓDULO 3
+
+### D O C T O R   P Y T H O N
+
+
+##### Introducción
+Este es un mini proyecto correspondiente a la evaluación final del Módulo 3 Fundamentos de Programación en Python, el cual consiste en crear un programa para registrar, ver, modificar, buscar y eliminar horas médicas.
+
+Su funcionamiento es en base a funciones tanto predefinidas como definidas por usuario, además del uso de una lista para contener la información ingresada.
+
+
+###### PROGRESO
+__24/02:__ Estructura básica definida, principales funciones definidas (4/5)
+
+###### COSAS A CORREGIR
+- no se muestra mensaje en caso de no haber tanto horas registradas en lista como en búsquedas
